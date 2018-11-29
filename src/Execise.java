7cff6918fb449221ad58b9d9f863db274d0dc479
mzinovyev <m.zinovyev@gmail.com>
2018-11-29 16:32:20 +0300

    init commit: class for execises

---
+++ src/Execise.java
@@ -0,0 +1,8 @@
+public class Execise {
+    public static boolean doubleExpression(double a, double b, double c) {
+        return a + b == c;
+    }
+    public static void main(String[] args) {
+        System.out.print("Hellow world");
+    }
+}
